11129643d941cd2c74846e8aa8c5a292408bddf8
Enock <tshims.enock31@gmail.com>
2026-02-08 17:47:36 -0500

    for loop

---
+++ JavaScript Basics/loops.js
@@ -0,0 +1,22 @@
+// for loop
+// for9("initialise"; "condition"; "increment or decrement"){
+//     "the code"
+// }
+
+for (let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+
+
+// while loop
+while ("a condition is true"){
+    "do smthg";
+}
+
+// example: 
+let counter = 0;
+while (counter < 10){ // as long as counter is less than 10, do the block
+    console.log(counter); // print the number of counter
+    counter++; //increase count by 1 each time
+}
