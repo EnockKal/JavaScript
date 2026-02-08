8b7f7c48dcb7c79fb1f63348d52d80bf185b3a93
Enock <tshims.enock31@gmail.com>
2026-02-08 17:11:30 -0500

    comsole log, variable, and datatype

---
+++ JavaScript Basics/Variable.js
@@ -0,0 +1,30 @@
+// let username;
+// username = "ENOCK";
+
+//or 
+
+let username = "Enock";
+let email = "enock@gmail.com"
+
+console.log(username);
+
+// Constante Variable
+// value that doens't change
+
+const firstname = "Enock"
+
+firstname = "Alex"; // TypeError: Can't reassign/change const variable
+
+// Naming
+// - Start with a letter,$, or _
+
+// some valide varaible names: 
+let a;
+let color;
+let _private;
+let $button;
+let getTop10;
+let a_large_name;
+let camelCase // when more than 1 word, the second word star with upperCase.
+
+// choose descriptive and meaningful names (have to describ the data the hold)
---
+++ JavaScript Basics/dataType.js
@@ -0,0 +1,13 @@
+// Strings
+let name = "name";
+
+// Numbers
+let age = 23;
+
+console.log("7" + 4); // this return a string: 74
+console.log(7 + 4); // this return a number or int : 11
+// that's is why we can't write everything as a string
+
+// Boolean (true or false)
+let isLoggedIn = false;
+console.log(age > 18); // return a boolean (true)
