6c560cf6b319d12a4fd97b8fe756e7e66842fc51
Enock <tshims.enock31@gmail.com>
2026-02-08 17:39:50 -0500

    if statement

---
+++ JavaScript Basics/ifStatement.js
@@ -0,0 +1,14 @@
+let age = 22;
+if(age > 18){ // this is true so it go to the next line. if it was false it would have noit executed the block
+    console.log("You are an adult") // it will print this line bcz the comparison statement was true
+}
+
+// an operation condition can be anything that can convert to a boolean
+
+if ("smthg work"){
+    "do smthg";
+} else if("smthg else work"){
+    "do smthing else";
+} else{
+    "then just do this"
+}
