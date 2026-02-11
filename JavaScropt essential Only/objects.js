f3d04118c1fd80ee4480aa4db174e0c92eafd7a7
Enock <tshims.enock31@gmail.com>
2026-02-11 15:13:52 -0500

    objects

--- JavaScropt essential Only/objects.js
+++ JavaScropt essential Only/objects.js
@@ -5,6 +5,7 @@ and methods (functions as property values), and are
 central to the language's object-based paradigm.
 */
 
+
 let user = {
     // properties
     firstName: "Enock",  // key, value
@@ -12,10 +13,33 @@ let user = {
     "is admin": false  // key, value
 };
 
-console.log(user.firstName); // Enock
 console.log(user.age); // 30
 
 // adding properties
 user.isSeniorCitizen = false; 
+user["movie lover"] = true;
+
+console.log(user);
 
 console.log(user["is admin"]);
+
+// modify properties
+user.age = 34;
+user["movie lover"] = false;
+
+// deleting properties
+//delete user["movie lover"];
+//delete user.age;
+console.log(user);
+
+// dynamic retriving values of a key.
+const someKey = "age";
+console.log(user[someKey]); // 34 for age.
+
+let car = prompt("which is your fav car");
+
+let favCars ={
+    [car]: 5
+}
+
+console.log(favCars);
