4435731f0d1f8aa12b2583dee8bd658c368ffbcf
Enock <tshims.enock31@gmail.com>
2026-02-11 17:31:19 -0500

    in operator

--- JavaScropt essential Only/Using Factory Function.js
+++ JavaScropt essential Only/Using Factory Function.js
@@ -40,3 +40,11 @@ profile.message();
 console.log(profile); // uper object (profile)
 console.log(profile.address); // nested object (address)
 console.log(profile.address.country); // usa
+
+// in operator
+// checking if profile has a salary:
+let isSalaryExist = "salary" in profile;
+console.log(isSalaryExist); // false. bcz there is no salary
+
+let isAdressExist = "address" in profile;
+console.log(isAdressExist); // true
